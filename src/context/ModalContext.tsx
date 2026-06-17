f506bcd3bf5eec69a97c26c05abf17730d4f7e6e
MarleMGN <m0b.torres23@gmail.com>
2026-06-17 11:26:06 -0300

    mobile sidebar

--- src/context/ModalContext.tsx
+++ src/context/ModalContext.tsx
@@ -4,12 +4,15 @@ import { createContext, useContext, useState } from "react"
 const ModalContext = createContext<{
   modalOpen: boolean
   setModalOpen: (value: boolean) => void
-}>({ modalOpen: false, setModalOpen: () => {} })
+  sidebarOpen: boolean
+  setSidebarOpen: (value: boolean) => void
+}>({ modalOpen: false, setModalOpen: () => {}, sidebarOpen: false, setSidebarOpen: () => {} })
 
 export function ModalProvider({ children }: { children: React.ReactNode }) {
   const [modalOpen, setModalOpen] = useState(false)
+  const [sidebarOpen, setSidebarOpen] = useState(false)
   return (
-    <ModalContext.Provider value={{ modalOpen, setModalOpen }}>
+    <ModalContext.Provider value={{ modalOpen, setModalOpen, sidebarOpen, setSidebarOpen }}>
       {children}
     </ModalContext.Provider>
   )
