c95d08e1bda19104497a5c69a358d62f032d11be
MarleMGN <m0b.torres23@gmail.com>
2026-07-04 12:35:35 -0700

    payment page

--- src/app/plan/page.tsx
+++ src/app/plan/page.tsx
@@ -1,10 +1,13 @@
-"use client"
+"use client";
 import React, { useState } from "react";
 import "./page.css";
 import Image from "next/image";
+import Footer from "@/components/footer";
 
 const page = () => {
-const [selectedPlan, setSelectedPlan] = useState("yearly")
+  const [selectedPlan, setSelectedPlan] = useState("yearly");
+  const [openAccordion, setOpenAccordion] = useState<number | null>(null);
+  
   return (
     <>
       <div className="wrapper wrapper__full">
@@ -30,56 +33,275 @@ const [selectedPlan, setSelectedPlan] = useState("yearly")
           </div>
         </div>
         <div className="container">
-            <div className="row">
-                <div className="plan__features--wrapper">
-                    <div className="plan__features">
-                        <figure className="plan__features--icon">
-                            <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 1024 1024" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M854.6 288.7c6 6 9.4 14.1 9.4 22.6V928c0 17.7-14.3 32-32 32H192c-17.7 0-32-14.3-32-32V96c0-17.7 14.3-32 32-32h424.7c8.5 0 16.7 3.4 22.7 9.4l215.2 215.3zM790.2 326L602 137.8V326h188.2zM320 482a8 8 0 0 0-8 8v48a8 8 0 0 0 8 8h384a8 8 0 0 0 8-8v-48a8 8 0 0 0-8-8H320zm0 136a8 8 0 0 0-8 8v48a8 8 0 0 0 8 8h184a8 8 0 0 0 8-8v-48a8 8 0 0 0-8-8H320z"></path></svg>
-                        </figure>
-                        <div className="plan__features--text"><b>Key ideas in a few min</b> with many books to read</div>
-                    </div>
-                    <div className="plan__features">
-                        <figure className="plan__features--icon">
-                            <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><g><path fill="none" d="M0 0H24V24H0z"></path><path d="M21 3v2c0 3.866-3.134 7-7 7h-1v1h5v7c0 1.105-.895 2-2 2H8c-1.105 0-2-.895-2-2v-7h5v-3c0-3.866 3.134-7 7-7h3zM5.5 2c2.529 0 4.765 1.251 6.124 3.169C10.604 6.51 10 8.185 10 10v1h-.5C5.358 11 2 7.642 2 3.5V2h3.5z"></path></g></svg>
-                        </figure>
-                        <div className="plan__features--text"><b>3 million</b> people growing with Summarist everyday</div>
-                    </div>
-                    <div className="plan__features">
-                        <figure className="plan__features--icon">
-                            <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 640 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M434.7 64h-85.9c-8 0-15.7 3-21.6 8.4l-98.3 90c-.1.1-.2.3-.3.4-16.6 15.6-16.3 40.5-2.1 56 12.7 13.9 39.4 17.6 56.1 2.7.1-.1.3-.1.4-.2l79.9-73.2c6.5-5.9 16.7-5.5 22.6 1 6 6.5 5.5 16.6-1 22.6l-26.1 23.9L504 313.8c2.9 2.4 5.5 5 7.9 7.7V128l-54.6-54.6c-5.9-6-14.1-9.4-22.6-9.4zM544 128.2v223.9c0 17.7 14.3 32 32 32h64V128.2h-96zm48 223.9c-8.8 0-16-7.2-16-16s7.2-16 16-16 16 7.2 16 16-7.2 16-16 16zM0 384h64c17.7 0 32-14.3 32-32V128.2H0V384zm48-63.9c8.8 0 16 7.2 16 16s-7.2 16-16 16-16-7.2-16-16c0-8.9 7.2-16 16-16zm435.9 18.6L334.6 217.5l-30 27.5c-29.7 27.1-75.2 24.5-101.7-4.4-26.9-29.4-24.8-74.9 4.4-101.7L289.1 64h-83.8c-8.5 0-16.6 3.4-22.6 9.4L128 128v223.9h18.3l90.5 81.9c27.4 22.3 67.7 18.1 90-9.3l.2-.2 17.9 15.5c15.9 13 39.4 10.5 52.3-5.4l31.4-38.6 5.4 4.4c13.7 11.1 33.9 9.1 45-4.7l9.5-11.7c11.2-13.8 9.1-33.9-4.6-45.1z"></path></svg>
-                        </figure>
-                        <div className="plan__features--text"><b>Precise recommendations</b> collections curated by experts</div>
-                    </div>
+          <div className="row">
+            <div className="plan__features--wrapper">
+              <div className="plan__features">
+                <figure className="plan__features--icon">
+                  <svg
+                    stroke="currentColor"
+                    fill="currentColor"
+                    strokeWidth="0"
+                    viewBox="0 0 1024 1024"
+                    height="1em"
+                    width="1em"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M854.6 288.7c6 6 9.4 14.1 9.4 22.6V928c0 17.7-14.3 32-32 32H192c-17.7 0-32-14.3-32-32V96c0-17.7 14.3-32 32-32h424.7c8.5 0 16.7 3.4 22.7 9.4l215.2 215.3zM790.2 326L602 137.8V326h188.2zM320 482a8 8 0 0 0-8 8v48a8 8 0 0 0 8 8h384a8 8 0 0 0 8-8v-48a8 8 0 0 0-8-8H320zm0 136a8 8 0 0 0-8 8v48a8 8 0 0 0 8 8h184a8 8 0 0 0 8-8v-48a8 8 0 0 0-8-8H320z"></path>
+                  </svg>
+                </figure>
+                <div className="plan__features--text">
+                  <b>Key ideas in a few min</b> with many books to read
                 </div>
-                <div className="section__title">Choose the plan that fits you</div>
-                <div className={`plan__card ${selectedPlan === "yearly" ? "plan__card--active" : "" }`} onClick={() => setSelectedPlan("yearly")}>
-                    <div className="plan__card--circle">
-                        {selectedPlan === "yearly" ? <div className="plan__card--dot"></div> : ""}
-                    </div>
-                    <div className="plan__card--content">
-                        <div className="plan__card--title">Premium Plus Yearly</div>
-                        <div className="plan__card--price">$99.99/year</div>
-                        <div className="plan__card--text">7-day free trial included</div>
-                    </div>
+              </div>
+              <div className="plan__features">
+                <figure className="plan__features--icon">
+                  <svg
+                    stroke="currentColor"
+                    fill="currentColor"
+                    strokeWidth="0"
+                    viewBox="0 0 24 24"
+                    height="1em"
+                    width="1em"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <g>
+                      <path fill="none" d="M0 0H24V24H0z"></path>
+                      <path d="M21 3v2c0 3.866-3.134 7-7 7h-1v1h5v7c0 1.105-.895 2-2 2H8c-1.105 0-2-.895-2-2v-7h5v-3c0-3.866 3.134-7 7-7h3zM5.5 2c2.529 0 4.765 1.251 6.124 3.169C10.604 6.51 10 8.185 10 10v1h-.5C5.358 11 2 7.642 2 3.5V2h3.5z"></path>
+                    </g>
+                  </svg>
+                </figure>
+                <div className="plan__features--text">
+                  <b>3 million</b> people growing with Summarist everyday
                 </div>
-                <div className="plan__card--separator">or</div>
-                <div className={`plan__card ${selectedPlan === "monthly" ? "plan__card--active" : "" }`} onClick={() => setSelectedPlan("monthly")}>
-                    <div className="plan__card--circle">
-                        {selectedPlan === "monthly" ? <div className="plan__card--dot"></div> : ""}
-                    </div>
-                    <div className="plan__card--content">
-                        <div className="plan__card--title">Premium Monthly</div>
-                        <div className="plan__card--price">$9.99/month</div>
-                        <div className="plan__card--text">No trial included</div>
-                    </div>
+              </div>
+              <div className="plan__features">
+                <figure className="plan__features--icon">
+                  <svg
+                    stroke="currentColor"
+                    fill="currentColor"
+                    strokeWidth="0"
+                    viewBox="0 0 640 512"
+                    height="1em"
+                    width="1em"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M434.7 64h-85.9c-8 0-15.7 3-21.6 8.4l-98.3 90c-.1.1-.2.3-.3.4-16.6 15.6-16.3 40.5-2.1 56 12.7 13.9 39.4 17.6 56.1 2.7.1-.1.3-.1.4-.2l79.9-73.2c6.5-5.9 16.7-5.5 22.6 1 6 6.5 5.5 16.6-1 22.6l-26.1 23.9L504 313.8c2.9 2.4 5.5 5 7.9 7.7V128l-54.6-54.6c-5.9-6-14.1-9.4-22.6-9.4zM544 128.2v223.9c0 17.7 14.3 32 32 32h64V128.2h-96zm48 223.9c-8.8 0-16-7.2-16-16s7.2-16 16-16 16 7.2 16 16-7.2 16-16 16zM0 384h64c17.7 0 32-14.3 32-32V128.2H0V384zm48-63.9c8.8 0 16 7.2 16 16s-7.2 16-16 16-16-7.2-16-16c0-8.9 7.2-16 16-16zm435.9 18.6L334.6 217.5l-30 27.5c-29.7 27.1-75.2 24.5-101.7-4.4-26.9-29.4-24.8-74.9 4.4-101.7L289.1 64h-83.8c-8.5 0-16.6 3.4-22.6 9.4L128 128v223.9h18.3l90.5 81.9c27.4 22.3 67.7 18.1 90-9.3l.2-.2 17.9 15.5c15.9 13 39.4 10.5 52.3-5.4l31.4-38.6 5.4 4.4c13.7 11.1 33.9 9.1 45-4.7l9.5-11.7c11.2-13.8 9.1-33.9-4.6-45.1z"></path>
+                  </svg>
+                </figure>
+                <div className="plan__features--text">
+                  <b>Precise recommendations</b> collections curated by experts
                 </div>
-                <div className="plan__card--cta">
-                    <span className="btn--wrapper">
-                        <button className="btn" style={{ width: "300px" }}>Start your free 7-day trial</button>
-                    </span>
-                    <div className="plan__disclaimer">Cancel your trial at any time before it ends, and you won't be charged.</div>
-                </div>
+              </div>
             </div>
+            <div className="section__title">Choose the plan that fits you</div>
+            <div
+              className={`plan__card ${selectedPlan === "yearly" ? "plan__card--active" : ""}`}
+              onClick={() => setSelectedPlan("yearly")}
+            >
+              <div className="plan__card--circle">
+                {selectedPlan === "yearly" ? (
+                  <div className="plan__card--dot"></div>
+                ) : (
+                  ""
+                )}
+              </div>
+              <div className="plan__card--content">
+                <div className="plan__card--title">Premium Plus Yearly</div>
+                <div className="plan__card--price">$99.99/year</div>
+                <div className="plan__card--text">
+                  7-day free trial included
+                </div>
+              </div>
+            </div>
+            <div className="plan__card--separator">or</div>
+            <div
+              className={`plan__card ${selectedPlan === "monthly" ? "plan__card--active" : ""}`}
+              onClick={() => setSelectedPlan("monthly")}
+            >
+              <div className="plan__card--circle">
+                {selectedPlan === "monthly" ? (
+                  <div className="plan__card--dot"></div>
+                ) : (
+                  ""
+                )}
+              </div>
+              <div className="plan__card--content">
+                <div className="plan__card--title">Premium Monthly</div>
+                <div className="plan__card--price">$9.99/month</div>
+                <div className="plan__card--text">No trial included</div>
+              </div>
+            </div>
+            <div className="plan__card--cta">
+              <span className="btn--wrapper">
+                <button className="btn" style={{ width: "300px" }}>
+                  {selectedPlan === "yearly" ? "Start your free 7-day trial" : "Start your first month" }
+                </button>
+              </span>
+              <div className="plan__disclaimer">
+                {selectedPlan === "yearly" ? "Cancel your trial at any time before it ends, and you won't be charged." : "30-day money back guarantee, no questions asked."}
+              </div>
+            </div>
+            <div className="faq__wrapper">
+              <div className="accordion__card">
+                <div
+                  className="accordion__header"
+                  onClick={() =>
+                    setOpenAccordion(openAccordion === 0 ? null : 0)
+                  }
+                >
+                  <div className="accordion__title">
+                    How does the free 7-day trial work?
+                  </div>
+                  <svg
+                    stroke="currentColor"
+                    fill="currentColor"
+                    strokeWidth="0"
+                    viewBox="0 0 16 16"
+                    className={`accordion__icon ${openAccordion === 0 ? "accordion__icon--rotate" : ""}`}
+                    height="1em"
+                    width="1em"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
+                    ></path>
+                  </svg>
+                </div>
+                <div
+                  className={`collapse ${openAccordion === 0 ? "show" : ""}`}
+                  style={{ maxHeight: openAccordion === 0 ? "100px" : "0px" }}
+                >
+                  <div className="accordion__body">
+                    Begin your complimentary 7-day trial with a Summarist annual
+                    membership. You are under no obligation to continue your
+                    subscription, and you will only be billed when the trial
+                    period expires. With Premium access, you can learn at your
+                    own pace and as frequently as you desire, and you may
+                    terminate your subscription prior to the conclusion of the
+                    7-day free trial.
+                  </div>
+                </div>
+              </div>
+              <div className="accordion__card">
+                <div
+                  className="accordion__header"
+                  onClick={() =>
+                    setOpenAccordion(openAccordion === 1 ? null : 1)
+                  }
+                >
+                  <div className="accordion__title">
+                    Can I switch subscriptions from monthly to yearly, or yearly
+                    to monthly?
+                  </div>
+                  <svg
+                    stroke="currentColor"
+                    fill="currentColor"
+                    strokeWidth="0"
+                    viewBox="0 0 16 16"
+                    className={`accordion__icon ${openAccordion === 1 ? "accordion__icon--rotate" : ""}`}
+                    height="1em"
+                    width="1em"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
+                    ></path>
+                  </svg>
+                </div>
+                <div
+                  className={`collapse ${openAccordion === 1 ? "show" : ""}`}
+                  style={{ maxHeight: openAccordion === 1 ? "100px" : "0px" }}
+                >
+                  <div className="accordion__body">
+                    While an annual plan is active, it is not feasible to switch
+                    to a monthly plan. However, once the current month ends,
+                    transitioning from a monthly plan to an annual plan is an
+                    option.
+                  </div>
+                </div>
+              </div>
+              <div className="accordion__card">
+                <div
+                  className="accordion__header"
+                  onClick={() =>
+                    setOpenAccordion(openAccordion === 2 ? null : 2)
+                  }
+                >
+                  <div className="accordion__title">
+                    What's included in the Premium plan?
+                  </div>
+                  <svg
+                    stroke="currentColor"
+                    fill="currentColor"
+                    strokeWidth="0"
+                    viewBox="0 0 16 16"
+                    className={`accordion__icon ${openAccordion === 2 ? "accordion__icon--rotate" : ""}`}
+                    height="1em"
+                    width="1em"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
+                    ></path>
+                  </svg>
+                </div>
+                <div
+                  className={`collapse ${openAccordion === 2 ? "show" : ""}`}
+                  style={{ maxHeight: openAccordion === 2 ? "100px" : "0px" }}
+                >
+                  <div className="accordion__body">
+                    Premium membership provides you with the ultimate Summarist
+                    experience, including unrestricted entry to many
+                    best-selling books high-quality audio, the ability to
+                    download titles for offline reading, and the option to send
+                    your reads to your Kindle.
+                  </div>
+                </div>
+              </div>
+              <div className="accordion__card">
+                <div
+                  className="accordion__header"
+                  onClick={() =>
+                    setOpenAccordion(openAccordion === 3 ? null : 3)
+                  }
+                >
+                  <div className="accordion__title">
+                    Can I cancel during my trial or subscription?
+                  </div>
+                  <svg
+                    stroke="currentColor"
+                    fill="currentColor"
+                    strokeWidth="0"
+                    viewBox="0 0 16 16"
+                    className={`accordion__icon ${openAccordion === 3 ? "accordion__icon--rotate" : ""}`}
+                    height="1em"
+                    width="1em"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
+                    ></path>
+                  </svg>
+                </div>
+                <div
+                  className={`collapse ${openAccordion === 3 ? "show" : ""}`}
+                  style={{ maxHeight: openAccordion === 3 ? "100px" : "0px" }}
+                >
+                  <div className="accordion__body">
+                    You will not be charged if you cancel your trial before its
+                    conclusion. While you will not have complete access to the
+                    entire Summarist library, you can still expand your
+                    knowledge with one curated book per day.
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <Footer />
         </div>
       </div>
     </>
