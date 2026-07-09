c2c1067df234e7bcc92108e19d5052d1051878aa
MarleMGN <m0b.torres23@gmail.com>
2026-07-08 17:15:48 -0700

    stripe payment setup

---
+++ src/app/account/standardPanel.tsx
@@ -0,0 +1,9 @@
+import "./panels.css";
+
+export const StandardPanel = () => {
+  return (
+    <div className="panel panel--standard">
+      Standard Member
+    </div>
+  );
+};
--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -20,3 +20,7 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app)
 export const googleProvider = new GoogleAuthProvider()
 export const db = getFirestore(app); 
+
+export const initFirebase = () => {
+  return app;
+};
